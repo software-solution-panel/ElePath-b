d996a36084c1acc42efa71d4581ae089a7ca9344
Chathumal Jayasinghe <chathumal.j@arpicoinsurance.com>
2025-01-24 21:24:43 +0530

    feat - initial commit

---
+++ src/main/java/com/swlc/elepath/elepath/ElepathApplication.java
@@ -0,0 +1,12 @@
+package com.swlc.elepath.elepath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElepathApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ElepathApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swlc/elepath/elepath/ElepathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swlc.elepath.elepath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElepathApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
