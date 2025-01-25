bad0e7cd686160555b1ec42a8f1cf113650f69d6
Chathumal Jayasinghe <chathumal.j@arpicoinsurance.com>
2025-01-25 20:15:17 +0530

    feat - update elephant entity

--- src/main/java/com/swlc/elepath/elepath/entity/Elephant.java
+++ src/main/java/com/swlc/elepath/elepath/entity/Elephant.java
@@ -1,6 +1,9 @@
 package com.swlc.elepath.elepath.entity;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
 
 @Entity
 @Table(name = "elephant")
@@ -8,6 +11,10 @@ public class Elephant {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long eid;
+    private Long location;
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastUpdateTime;
 
 
 }
