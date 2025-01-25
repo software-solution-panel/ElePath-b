53959150b655d0838355fad5131de04388480f8b
Chathumal Jayasinghe <chathumal.j@arpicoinsurance.com>
2025-01-25 20:16:04 +0530

    feat - added Lombok

--- src/main/java/com/swlc/elepath/elepath/entity/Elephant.java
+++ src/main/java/com/swlc/elepath/elepath/entity/Elephant.java
@@ -1,10 +1,16 @@
 package com.swlc.elepath.elepath.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.util.Date;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @Entity
 @Table(name = "elephant")
 public class Elephant {
