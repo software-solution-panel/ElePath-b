4c8c3b7f6321a6b1d8f222a4398060ba5333d0fe
Chathumal Jayasinghe <chathumal.j@arpicoinsurance.com>
2025-01-25 20:26:41 +0530

    feat - update elephant controller

--- src/main/java/com/swlc/elepath/elepath/controller/ElephantController.java
+++ src/main/java/com/swlc/elepath/elepath/controller/ElephantController.java
@@ -1,10 +1,21 @@
 package com.swlc.elepath.elepath.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.swlc.elepath.elepath.dto.ElephantDTO;
+import com.swlc.elepath.elepath.service.ElephantService;
+import com.swlc.elepath.elepath.util.StandardResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/elephant")
 public class ElephantController {
 
+    @Autowired
+    private ElephantService elephantService;
+    @PostMapping("/addElephant")
+    private ResponseEntity<StandardResponse<?>> addRate(@RequestBody ElephantDTO dto) {
+        return elephantService.addElephant(dto);
+    }
 }
