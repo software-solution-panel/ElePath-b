13fe8b6a761cd84205c0f0dcacf035fa4870900c
Chathumal Jayasinghe <chathumal.j@arpicoinsurance.com>
2025-01-25 20:10:49 +0530

    feat - update elephant repository

--- src/main/java/com/swlc/elepath/elepath/reporitory/ElephantRepository.java
+++ src/main/java/com/swlc/elepath/elepath/reporitory/ElephantRepository.java
@@ -1,6 +1,7 @@
 package com.swlc.elepath.elepath.reporitory;
 
+import com.swlc.elepath.elepath.entity.Elephant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ElephantRepository {
+public interface ElephantRepository extends JpaRepository<Elephant,Long> {
 }
