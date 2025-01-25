fb51e331cfd0cdbdb5f0c8a03b1625b6ba3360df
Chathumal Jayasinghe <chathumal.j@arpicoinsurance.com>
2025-01-25 20:19:26 +0530

    feat - added elephant DTO

--- src/main/java/com/swlc/elepath/elepath/dto/ElephantDTO.java
+++ src/main/java/com/swlc/elepath/elepath/dto/ElephantDTO.java
@@ -1,4 +1,12 @@
 package com.swlc.elepath.elepath.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class ElephantDTO {
+    private Long location;
 }
