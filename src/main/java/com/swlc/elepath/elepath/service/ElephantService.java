ce5d6969a1c2cefa93385a4dcfae6672f4597178
Chathumal Jayasinghe <chathumal.j@arpicoinsurance.com>
2025-01-25 20:24:33 +0530

    feat - added elephant service

--- src/main/java/com/swlc/elepath/elepath/service/ElephantService.java
+++ src/main/java/com/swlc/elepath/elepath/service/ElephantService.java
@@ -1,4 +1,10 @@
 package com.swlc.elepath.elepath.service;
 
+import com.swlc.elepath.elepath.dto.ElephantDTO;
+import com.swlc.elepath.elepath.util.StandardResponse;
+import org.springframework.http.ResponseEntity;
+
 public interface ElephantService {
+    ResponseEntity<StandardResponse<?>> addElephant(ElephantDTO dto);
+
 }
--- src/main/java/com/swlc/elepath/elepath/service/impl/ElephantServiceImpl.java
+++ src/main/java/com/swlc/elepath/elepath/service/impl/ElephantServiceImpl.java
@@ -1,6 +1,33 @@
 package com.swlc.elepath.elepath.service.impl;
 
+import com.swlc.elepath.elepath.dto.ElephantDTO;
+import com.swlc.elepath.elepath.entity.Elephant;
+import com.swlc.elepath.elepath.reporitory.ElephantRepository;
 import com.swlc.elepath.elepath.service.ElephantService;
+import com.swlc.elepath.elepath.util.StandardResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Transactional
 public class ElephantServiceImpl implements ElephantService {
+
+    @Autowired
+    private ElephantRepository repository;
+
+    @Override
+    public ResponseEntity<StandardResponse<?>> addElephant(ElephantDTO dto) {
+        try {
+            Elephant elephant = new Elephant();
+            elephant.setLocation(dto.getLocation());
+            repository.save(elephant);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(new StandardResponse<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Cannot save","Internal Server Problem"), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(new StandardResponse<>(200, "Elephant Save Success", "OK"), HttpStatus.OK);
+    }
 }
