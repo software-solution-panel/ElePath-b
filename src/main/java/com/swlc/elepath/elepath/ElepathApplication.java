ff7173f975b0a6b41e67e725a74ae185f35bbbdd
Chathumal Jayasinghe <chathumal.j@arpicoinsurance.com>
2025-01-25 19:57:22 +0530

    feat - add ModelMapper

--- src/main/java/com/swlc/elepath/elepath/ElepathApplication.java
+++ src/main/java/com/swlc/elepath/elepath/ElepathApplication.java
@@ -1,12 +1,20 @@
 package com.swlc.elepath.elepath;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ElepathApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(ElepathApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
     }
 
 }
--- src/main/java/com/swlc/elepath/elepath/entity/Elephant.java
+++ src/main/java/com/swlc/elepath/elepath/entity/Elephant.java
@@ -1,4 +1,13 @@
 package com.swlc.elepath.elepath.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "elephant")
 public class Elephant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long eid;
+
+
 }
