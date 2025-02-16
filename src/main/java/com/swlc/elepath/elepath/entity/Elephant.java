2520a0eeeba9d0495ccc8a2f2ca5b0be0f4f91eb
Chathumal Jayasinghe <chathumal.j@arpicoinsurance.com>
2025-02-16 06:40:26 +0530

    feat - occur an error in Lombok

--- src/main/java/com/swlc/elepath/elepath/entity/Elephant.java
+++ src/main/java/com/swlc/elepath/elepath/entity/Elephant.java
@@ -9,9 +9,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import java.time.LocalDateTime;
 import java.util.Date;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
+
 @Entity
 @Table(name = "elephant")
 public class Elephant {
@@ -24,4 +22,56 @@ public class Elephant {
     @CreationTimestamp
     @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime lastUpdateTime;
+
+    public Elephant(Long eid, Double latitude, Double longitude, String imagePath, LocalDateTime lastUpdateTime) {
+        this.eid = eid;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.imagePath = imagePath;
+        this.lastUpdateTime = lastUpdateTime;
+    }
+
+    public Elephant() {
+    }
+
+    public void setEid(Long eid) {
+        this.eid = eid;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public void setLastUpdateTime(LocalDateTime lastUpdateTime) {
+        this.lastUpdateTime = lastUpdateTime;
+    }
+
+    public Long getEid() {
+        return eid;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public LocalDateTime getLastUpdateTime() {
+        return lastUpdateTime;
+    }
+
 }
--- src/main/java/com/swlc/elepath/elepath/util/StandardResponse.java
+++ src/main/java/com/swlc/elepath/elepath/util/StandardResponse.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
+
 public class StandardResponse<T> {
     private int code;
     private String message;
@@ -15,4 +13,36 @@ public class StandardResponse<T> {
         this.code = code;
         this.message = message;
     }
+
+    public StandardResponse(int code, String message, T response) {
+        this.code = code;
+        this.message = message;
+        this.response = response;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public T getResponse() {
+        return response;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setResponse(T response) {
+        this.response = response;
+    }
+
+
 }
