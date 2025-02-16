6c0a856b9255df8f8eae90ac1cfe197216db51e6
Chathumal Jayasinghe <chathumal.j@arpicoinsurance.com>
2025-02-16 06:12:48 +0530

    feat - update elephant controller

--- src/main/java/com/swlc/elepath/elepath/controller/ElephantController.java
+++ src/main/java/com/swlc/elepath/elepath/controller/ElephantController.java
@@ -6,6 +6,7 @@ import com.swlc.elepath.elepath.util.StandardResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @CrossOrigin
@@ -15,7 +16,9 @@ public class ElephantController {
     @Autowired
     private ElephantService elephantService;
     @PostMapping("/addElephant")
-    private ResponseEntity<StandardResponse<?>> addRate(@RequestBody ElephantDTO dto) {
-        return elephantService.addElephant(dto);
+    private ResponseEntity<StandardResponse<?>> addRate(@RequestParam("image") MultipartFile image,
+                                                        @RequestParam("latitude") Double latitude,
+                                                        @RequestParam("longitude") Double longitude) {
+        return elephantService.addElephant(image, latitude, longitude);
     }
 }
