ad8c594c728479767eba1127f887e4cb7bbe836b
Chathumal Jayasinghe <chathumal.j@arpicoinsurance.com>
2025-02-16 16:23:14 +0530

    feat - remove Lombok

--- src/main/java/com/swlc/elepath/elepath/entity/User.java
+++ src/main/java/com/swlc/elepath/elepath/entity/User.java
@@ -6,11 +6,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
+
 @Entity
 @Table(name = "user")
 public class User {
@@ -23,6 +22,57 @@ public class User {
     private String mobile;
     @CreationTimestamp
     @Temporal(TemporalType.TIMESTAMP)
-    private Date creadt;
+    private LocalDateTime creadt;
 
+    public User() {
+    }
+
+    public User(Long uid, String email, String mobile, LocalDateTime creadt) {
+        this.uid = uid;
+        this.email = email;
+        this.mobile = mobile;
+        this.creadt = creadt;
+    }
+
+    public Long getUid() {
+        return uid;
+    }
+
+    public void setUid(Long uid) {
+        this.uid = uid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public LocalDateTime getCreadt() {
+        return creadt;
+    }
+
+    public void setCreadt(LocalDateTime creadt) {
+        this.creadt = creadt;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "uid=" + uid +
+                ", email='" + email + '\'' +
+                ", mobile='" + mobile + '\'' +
+                ", creadt=" + creadt +
+                '}';
+    }
 }
